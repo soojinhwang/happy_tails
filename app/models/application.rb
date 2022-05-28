0e1c3b9c69ab6b8f160f2251baa92bbb7f324fa4
alessia bacigalupo <bacigalupoalessia@gmail.com>
2022-05-28 15:48:48 +0200

    model app validation

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,4 +1,9 @@
 class Application < ApplicationRecord
   belongs_to :user
   belongs_to :pet
+
+  validates :date, presence: true
+  validates :approved, inclusion: { in: [true, false] }
+  validates :user_id, presence: true
+  validates :pet_id, presence: true
 end
