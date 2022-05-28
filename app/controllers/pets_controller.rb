cdc044af390725187e3c0a6d1c499401bc29179a
Josef Hare-Brown <josefhb@hotmail.com>
2022-05-28 16:37:13 +0100

    begun index

---
+++ app/controllers/pets_controller.rb
@@ -0,0 +1,45 @@
+class PetsController < ApplicationController
+  def index
+    @pets = Pet.all
+  end
+
+#   def show
+#     @pet = Pet.find(params[:id])
+#   end
+
+#   def new
+#     @pet = Pet.new
+#   end
+
+#   def create
+#     @pet = Pet.new(pet_params)
+#     @pet.user = current_user
+#     if @pet.save
+#       redirect_to pet_path(@pet)
+#     else
+#       render :new
+#     end
+
+#     def edit
+#     end
+
+#     def update
+#       @pet.update(pet_params)
+#       redirect_to pet_path(@pet)
+#     end
+
+#     def destroy
+#       @pet.destroy
+#       redirect_to pets_path
+#     end
+
+# private
+
+#     def set_pet
+#       @pet = Pet.find(params[:id])
+#     end
+
+#     def pet_params
+#       params.require(:pet).permit(:name, :species, :description, :location, :breed, :sex, :age, :colour, :medical_conditions,:hours_alone, :children_friendly )
+#     end
+end
