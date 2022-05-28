de8e0c79de200c1a0111c99f29ce88ce5a125083
Josef Hare-Brown <josefhb@hotmail.com>
2022-05-28 14:34:56 +0100

    added saved validations

--- app/models/shelter_info.rb
+++ app/models/shelter_info.rb
@@ -1,9 +1,11 @@
 class ShelterInfo < ApplicationRecord
   belongs_to :user
   has_many :pets, dependent: :destroy
-  has_many :applications, dependent: :destroy
+
 
 
   validates :name, presence: true
   validates :name, length: { minimum: 2}
+  validates :address, presence: true
+  validates :user_id, presence: true
 end
