d9dffb941d6e4d8bf3d289ebc2c9d07f90a68cbd
alessia bacigalupo <bacigalupoalessia@gmail.com>
2022-06-07 20:50:00 +0200

    adding details to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_28_161127) do
+ActiveRecord::Schema.define(version: 2022_06_07_184121) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,6 +65,17 @@ ActiveRecord::Schema.define(version: 2022_05_28_161127) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.integer "age"
+    t.boolean "shelter"
+    t.text "biography"
+    t.string "type_of_dwelling"
+    t.boolean "outdoor_space"
+    t.string "other_pets"
+    t.string "children"
+    t.integer "hours_alone"
+    t.text "previous_experience"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
