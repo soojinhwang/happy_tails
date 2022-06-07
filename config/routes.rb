0807f9f0d1af5757a387a300acbc72319337a9e6
Samuel Gebru <samueltgebru@gmail.com>
2022-06-07 20:43:15 +0100

    created the routes for pets, applicatons and shelters

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :pets
-  #resources :shelters
+  resources :pets do
+    resources :applications
+  end
+
+  resources :shelters
+
 end
