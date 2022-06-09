b9092f598de9f98e0f8794fe4ca007359e9f4e78
alessia bacigalupo <bacigalupoalessia@gmail.com>
2022-06-09 20:06:34 +0200

    conflict solved

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,21 +25,21 @@ User.create!(email: "shelter2022@gmail.com",
 
 puts "user shelter created.."
 
-puts "creating pets.."
+# puts "creating pets.."
 
-Pet.create!(adoption_status: "Available for adoption"
-             name: "Pixel"
-             species: "Dog"
-             breed: "crossbreed"
-             sex: "male"
-             age: "six months"
-             colour: "black"
-             description: "Pixel is a typical playful pup who is a sweet little boy."
-             outdoor_space: true
-             pet_friendly: "Yes"
-             children_friendly: "Yes"
-             hours_alone: 3
-             medical_conditions: "Good health condition"
-)
+# Pet.create! m (adoption_status: "Available for adoption"
+#             name: "Pixel"
+#             species: "Dog"
+#             breed: "crossbreed"
+#             sex: "male"
+#              age: "six months"
+#              colour: "black"
+#              description: "Pixel is a typical playful pup who is a sweet little boy."
+#              outdoor_space: true
+#              pet_friendly: "Yes"
+#              children_friendly: "Yes"
+#              hours_alone: 3
+#              medical_conditions: "Good health condition"
+# )
 
-puts "pet created.."
+# puts "pet created.."
