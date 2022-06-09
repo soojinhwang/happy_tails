9bb64338a72ea21ae64cfdfe1f24693bd2066b65
Soojin Hwang <soojiny.hwang@gmail.com>
2022-06-09 21:04:47 +0100

    seed file edits

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,26 +6,48 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-puts "creating user who is a shelter... "
+puts "Cleaning database..."
+User.destroy_all
+Shelter.destroy_all
+Pet.destroy_all
+Application.destroy_all
 
-user1 = User.create!(email: "shelter2022@gmail.com",
-             password: "shelter2022",
-             first_name: "Jane",
-             last_name: "Hopper",
-             age: 30,
-            #  shelter: true,
-             biography: "A shelter in Shoreditch looking to use Happy Tails.",
-             type_of_dwelling: "House",
-             outdoor_space: true,
-             other_pets: "yes I have plenty",
-             children: "none",
-             hours_alone: 1,
-             previous_experience: "yes spent 10 years working with pets"
-             )
+puts "creating users... "
 
-puts "user shelter created.."
+jane = User.create!(email: "jane@gmail.com",
+                    password: "123456",
+                    first_name: "Jane",
+                    last_name: "Hopper")
+                    # age: 30,
+                    # biography: "I'm a volunteer",
+                    # type_of_dwelling: "House",
+                    # outdoor_space: true,
+                    # other_pets: "yes I have plenty",
+                    # children: "none",
+                    # hours_alone: 1,
+                    # previous_experience: "yes spent 10 years working with pets"
 
-shelter1 = Shelter.create!
+puts "Created jane"
+
+mike = User.create!(email: "mike@gmail.com",
+                    password: "123456",
+                    first_name: "Mike",
+                    last_name: "Smith",
+                    age: "26",
+                    biography: "My partner and I are looking for a dog to adopt",
+                    type_of_dwelling: "Flat",
+                    outdoor_space: false,
+                    other_pets: "I have a cat",
+                    children: "None",
+                    hours_alone: 4,
+                    previous_experience: "I've had dogs since I was 5.")
+
+puts "Created mike"
+puts "All users created"
+
+puts "creating users... "
+
+shoreditch = Shelter.create!()
 
 # puts "creating pets.."
 
