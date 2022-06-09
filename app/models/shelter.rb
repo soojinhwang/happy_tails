abe58e8a36e0b99e3b5d18602e4b1e8d26433314
Soojin Hwang <soojiny.hwang@gmail.com>
2022-06-09 20:28:47 +0100

    edited shelter validation to match schema

--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -1,6 +1,9 @@
 class Shelter < ApplicationRecord
   belongs_to :user
   has_many :pets, dependent: :destroy
+  has_many :applications, through: :pets
+
   validates :name, presence: true, length: { minimum: 2 }
   validates :address, presence: true
+  validates :description, presence: true, length: { minimum: 10 }
 end
