7feaa471a443875a0c79dae85ecf4883295d24c6
Soojin Hwang <soojiny.hwang@gmail.com>
2022-06-09 19:21:58 +0100

    devise applications controller authentications

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,18 +9,18 @@
 puts "creating user who is a shelter... "
 
 User.create!(email: "shelter2022@gmail.com",
-            password: "shelter2022",
-            first_name: "Cat",
-            last_name: "Dog",
-            age: 30,
-            shelter: true,
-            biography: "A shelter in Shoreditch looking to use Happy tails.",
-            type_of_dwelling: "a shop",
-            outdoor_space: true,
-            other_pets: "yes i have plenty",
-            children: "none",
-            hours_alone: 1,
-            previous_experience: "yes spent 10 years working with pets",
-            )
+             password: "shelter2022",
+             first_name: "Jane",
+             last_name: "Hopper",
+             age: 30,
+             shelter: true,
+             biography: "A shelter in Shoreditch looking to use Happy tails.",
+             type_of_dwelling: "House",
+             outdoor_space: true,
+             other_pets: "yes I have plenty",
+             children: "none",
+             hours_alone: 1,
+             previous_experience: "yes spent 10 years working with pets"
+             )
 
 puts "user shelter created.."
