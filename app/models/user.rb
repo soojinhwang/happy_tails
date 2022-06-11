2506de54f03beddc4f31fc32a1a2303057e0a1ef
Soojin Hwang <soojiny.hwang@gmail.com>
2022-06-11 12:19:33 +0100

    now can create user without all the detailed fields

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,12 +12,16 @@ class User < ApplicationRecord
   validates :password, presence: true
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :age, numericality: { only_integer: true }
-  validates :biography, length: { minimum: 10 }
+
+  validates :age, numericality: { only_integer: true }, unless: ->(x) { x.age.blank? }
+  validates :biography, length: { minimum: 10 }, unless: ->(x) { x.biography.blank? }
   validates :type_of_dwelling, inclusion: { in: %w(House Flat Farm),
-                                            message: "%{value} is not a valid type of dwelling" }
-  validates  :outdoor_space, inclusion: { in: [true, false],
-                                          message: "%{value} is not a valid option" }
-  validates  :hours_alone, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 24 }
-  validates  :previous_experience, length: { minimum: 10 }
+                                            message: "%{value} is not a valid type of dwelling" },
+                               unless: ->(x) { x.type_of_dwelling.blank? }
+  validates :outdoor_space, inclusion: { in: [true, false],
+                                          message: "%{value} is not a valid option" },
+                            unless: ->(x) { x.outdoor_space.blank? }
+  validates  :hours_alone, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 24 },
+                           unless: ->(x) { x.hours_alone.blank? }
+  validates  :previous_experience, length: { minimum: 10 }, unless: ->(x) { x.previous_experience.blank? }
 end
