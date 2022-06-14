81117c061cd8b2b705b2a948d38f28c611a3f960
Soojin Hwang <soojiny.hwang@gmail.com>
2022-06-14 22:22:49 +0100

    edited buttons CSS slightly

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,7 +1,6 @@
 class PetsController < ApplicationController
 
   skip_before_action :authenticate_user!, only: [:index, :show]
-
   before_action :set_pet, only: [ :show, :edit, :update, :detroy ]
 
   def index
