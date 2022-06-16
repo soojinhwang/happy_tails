9d13042f4968a895f6759471c3aee6639605deee
Samuel Gebru <samueltgebru@gmail.com>
2022-06-16 10:51:00 +0100

    adding/updating a pet and showing interest journey completed

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,7 +1,7 @@
 class PetsController < ApplicationController
 
   skip_before_action :authenticate_user!, only: [:index, :show]
-  before_action :set_pet, only: [ :show, :edit, :update, :detroy ]
+  before_action :set_pet, only: [ :show, :edit, :update, :destroy ]
 
   def index
     if params[:query].present?
@@ -36,7 +36,8 @@ class PetsController < ApplicationController
     end
   end
 
-  def edit; end
+  def edit
+  end
 
   def update
     @pet.update(pet_params)
