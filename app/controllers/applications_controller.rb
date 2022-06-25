2a4641bb2492758df501f81a7e2f702f238a384b
Soo-Jin <96022378+soojinhwang@users.noreply.github.com>
2022-06-25 14:50:49 +0100

    Revert "Showpage front end"

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -21,6 +21,8 @@ class ApplicationsController < ApplicationController
     @application.approved = false
     @application.reviewed = false
 
+
+
     @pet = Pet.find(params[:pet_id])
     @application.pet = @pet
     @application.user = current_user
