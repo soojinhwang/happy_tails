452a5042c7adb4f738c6fa98e0e65dab04aad5ef
Samuel Gebru <samueltgebru@gmail.com>
2022-06-30 15:02:59 +0100

    added the seed file for mike

--- db/seeds.rb
+++ db/seeds.rb
@@ -794,11 +794,15 @@ app1 = Application.create!(user: derek,
                            approved: true,
                            reviewed: true)
 
+puts "application 1 created!"
+
 app2 = Application.create!(user: derek,
                            pet: spike,
                            date: Date.today,
                            approved: false,
                            reviewed: true)
+
+puts "application 2 created!"
 
 app3 = Application.create!(user: jenny,
                            pet: memphis,
@@ -806,10 +810,31 @@ app3 = Application.create!(user: jenny,
                            approved: false,
                            reviewed: true)
 
+puts "application 3 created!"
+
 app4 = Application.create!(user: jenny,
                            pet: lila,
                            date: Date.today,
                            approved: false,
                            reviewed: true)
+
+puts "application 4 created!"
+
+app5 = Application.create!(user: mike,
+                            pet: blanca,
+                            date: DateTime.parse("04/01/2022"),
+                            approved: false,
+                            reviewed: true)
+
+puts "application 5 created!"
+
+app6 = Application.create!(user: mike,
+                           pet: spike,
+                           date: DateTime.parse("04/01/2022"),
+                           approved: true,
+                           reviewed: true)
+
+puts "application 6 created!"
+
 
 puts "All applications created"
